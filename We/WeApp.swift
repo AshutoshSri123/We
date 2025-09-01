2b4174dccb8c4bf534a44cfcc38689f3c7701b1c
AshutoshSri123 <ashu.61003@gmail.com>
2025-09-01 22:55:05 +0530

    Initial Commit

---
+++ We/WeApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeApp.swift
+//  We
+//
+//  Created by Ashutosh Srivastava on 9/1/25.
+//
+
+import SwiftUI
+
+@main
+struct WeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WeTests/WeTests.swift
@@ -0,0 +1,17 @@
+//
+//  WeTests.swift
+//  WeTests
+//
+//  Created by Ashutosh Srivastava on 9/1/25.
+//
+
+import Testing
+@testable import We
+
+struct WeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
